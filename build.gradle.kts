f3a5aea60495c57de8b02b472a4d49db305a98a7
helderlinhares <heldercomp@yahoo.com.br>
2022-05-07 20:01:44 -0300

    Validation fix and tests.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,16 +23,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
-    implementation("javax.validation:validation-api:2.0.1.Final")
     runtimeOnly("mysql:mysql-connector-java")
     runtimeOnly("com.h2database:h2")
     testImplementation("junit:junit")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("com.google.code.gson:gson:2.8.6")
+    testImplementation("com.google.code.gson:gson:2.9.0")
 }
 
 dependencyManagement {
